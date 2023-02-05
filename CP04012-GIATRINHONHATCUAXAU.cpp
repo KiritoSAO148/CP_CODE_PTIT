6ec55c531a0bb53e86bb4eb83315642729797bc8
B20DCPT094 <wuochuy02@gmail.com>
2023-02-05 23:16:09 +0700

    CP04012-GIATRINHONHATCUAXAU.cpp

---
+++ CP04012-GIATRINHONHATCUAXAU.cpp
@@ -0,0 +1,66 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef vector<ll> vl;
+typedef vector<vl> vvl;
+
+#define ms(s,n) memset(s,n,sizeof(s))
+#define all(a) a.begin(),a.end()
+#define sz(a) int((a).size())
+#define f0(i,n) for (int i=0; i<n; i++)
+#define f1(i,n) for (int i=1; i<=n; i++)
+#define FastIO ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define TC() int t; cin >> t; while (t--)
+#define el cout << "\n"
+#define pb push_back
+#define pf push_front
+#define fi first
+#define se second
+#define maxn 
+
+const int MOD = (int) 1e9+7;
+
+void FileIO(){
+    #ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    #endif
+}
+
+int main(){
+    FileIO();
+    FastIO;
+    TC(){
+    	int k; cin >> k;
+    	string s; cin >> s;
+    	int cnt[256];
+    	ms(cnt, 0);
+    	for (char x : s) cnt[x]++;
+    	priority_queue <int> pq;
+    	f0 (i,256){
+    		if (cnt[i]) pq.push(cnt[i]);
+    	}
+    	int ans = 0;
+    	while (k--){
+    		int top = pq.top();
+    		pq.pop();
+    		--top;
+    		pq.push(max(0, top));
+    	}
+    	while (!pq.empty()){
+    		int top = pq.top();
+    		ans += top * top;
+    		pq.pop();
+    	}
+    	cout << ans; el;
+    }
+    return 0;
+}
